6c3dcd34d560ddd400d53a937a5de2d06353da7e
Manikanta Jalla <manikanta.jalla8@gmail.com>
2024-07-06 16:18:55 -0400

    added

--- src/main/java/com/company/model/Product.java
+++ src/main/java/com/company/model/Product.java
@@ -1,14 +1,15 @@
 package com.company.model;
 
-import org.springframework.data.annotation.ID;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("Product")
 public class Product {
-	
+
 	private Product() {
 		super();
 	}
-	
+
 	public Product(Long id, String productName, Integer price, String productDesc) {
 		super();
 		this.id = id;
@@ -17,33 +18,40 @@ public class Product {
 		this.productDesc = productDesc;
 	}
 
-	@ID
+	@Id
 	private Long id;
 	private String productName;
 	private Integer price;
 	private String productDesc;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getProductName() {
 		return productName;
 	}
+
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
+
 	public Integer getPrice() {
 		return price;
 	}
+
 	public void setPrice(Integer price) {
 		this.price = price;
 	}
+
 	public String getProductDesc() {
 		return productDesc;
 	}
+
 	public void setProductDesc(String productDesc) {
 		this.productDesc = productDesc;
 	}
